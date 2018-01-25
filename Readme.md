3697abdf4477ed63c57547acd0ad3bea91654979
Tommy Parnell <tommy@terribledev.io>
2018-01-24 23:52:35 -0500

    init

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is a simple c# repl powered by roslyn
+
+![](Capture.PNG)
--- SharpRepl/Program.cs
+++ SharpRepl/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace SharpRepl
 {
@@ -6,7 +10,31 @@ namespace SharpRepl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach(var line in banner)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine();
+            var state = CSharpScript.RunAsync("2+2").Result; //prime the engine
+            while (true)
+            {
+                try
+                {
+                    Console.Write('>');
+                    var input = Console.ReadLine();
+                    state = state.ContinueWithAsync(input).Result;
+                    if (state.ReturnValue != null && !string.IsNullOrWhiteSpace(state.ReturnValue.ToString()))
+                    {
+                        Console.WriteLine(state.ReturnValue);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
+        public static string[] banner = File.ReadAllLines(Path.Combine(Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName, "Banner.txt"));
     }
 }
